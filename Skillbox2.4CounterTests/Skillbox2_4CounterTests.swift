dd09327133d69edb2b4ddace3e26251cc0930d87
Artem Akopian <artem7498@gmail.com>
2020-11-02 16:19:21 +1000

    Initial Commit

---
+++ Skillbox2.4CounterTests/Skillbox2_4CounterTests.swift
@@ -0,0 +1,33 @@
+//
+//  Skillbox2_4CounterTests.swift
+//  Skillbox2.4CounterTests
+//
+//  Created by Артём on 11/2/20.
+//
+
+import XCTest
+@testable import Skillbox2_4Counter
+
+class Skillbox2_4CounterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
